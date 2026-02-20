f2cef4248da08aee5aef337dcdcac12cbe02b872
diego <diego@LAPTOP-TLN7M1RT>
2026-02-20 10:48:34 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace primera_prueba_con_git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
